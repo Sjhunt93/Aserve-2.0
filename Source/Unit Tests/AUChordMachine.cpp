090ba570a0616be8c0a8b4d71f318ec7961b72c1
sam hunt <samuel.hunt@uwe.ac.uk>
2020-12-10 16:32:29 +0000

    Fixed a bug

--- Source/Unit Tests/AUChordMachine.cpp
+++ Source/Unit Tests/AUChordMachine.cpp
@@ -93,7 +93,7 @@ void AUTChordMachine::runningUnitTest ()
         
         std::sort(freqReceived.begin(), freqReceived.end());
         std::sort(testSetFreq.begin(), testSetFreq.end());
-        for (int j = 0; j < testSetFreq.size() && freqReceived.size(); j++) {
+        for (int j = 0; j < testSetFreq.size() && j < freqReceived.size(); j++) {
             if ((int)testSetFreq[j] != (int)freqReceived[j]) {
                 currentState = eTestState::eEndedFail;
 //                errorMessages += "Freqency received does not match what was expected for note number: " + String(note) + "\n";
